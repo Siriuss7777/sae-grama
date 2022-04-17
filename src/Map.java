eaba72ddd96e351e5974c12d5ba5dc07a2f2bc06
Mateo <mateo.guenot2003@gmail.com>
2022-04-17 12:45:56 +0200

    Floyd Warshall :)

--- src/Map.java
+++ src/Map.java
@@ -25,7 +25,7 @@ public class Map {
 
     public Node createNode(String type, String name) {
         Node tempNode;
-        if((tempNode = this.getNodeFromString(type+","+name)) == null){
+        if ((tempNode = this.getNodeFromString(type + "," + name)) == null) {
             tempNode = new Node(type, name);
             this.nodes.add(tempNode);
         }
@@ -96,10 +96,10 @@ public class Map {
         return count;
     }
 
-    public Node getNodeFromString(String string){
+    public Node getNodeFromString(String string) {
         Node returnedNode = null;
-        for(Node node: this.nodes){
-            if(node.toString().equals(string)){
+        for (Node node : this.nodes) {
+            if (node.toString().equals(string)) {
                 returnedNode = node;
             }
         }
@@ -110,7 +110,7 @@ public class Map {
     /*------------------------------------- OPÉRATIONS SUR LES LIENS -------------------------------------*/
     /*----------------------------------------------------------------------------------------------------*/
 
-    public void addLink(Link link){
+    public void addLink(Link link) {
         this.links.add(link);
     }
 
@@ -215,12 +215,12 @@ public class Map {
         fromNode.setDistance(0);
         untreatedNodes.add(fromNode);
 
-        while(!untreatedNodes.isEmpty()){
+        while (!untreatedNodes.isEmpty()) {
             Node currentNode = getClosestNode(untreatedNodes);
             untreatedNodes.remove(currentNode);
-            for(Node neighbourNode : currentNode.getNeighbourNodes()){
+            for (Node neighbourNode : currentNode.getNeighbourNodes()) {
                 length = neighbourNode.getClosestNeighbour(currentNode).getDistance();
-                if(!treatedNodes.contains(neighbourNode)){
+                if (!treatedNodes.contains(neighbourNode)) {
                     updateDistances(neighbourNode, length, currentNode);
                     untreatedNodes.add(neighbourNode);
                 }
@@ -237,7 +237,7 @@ public class Map {
         Node returnedNode = null;
         for (Node node : neighboursList) {
             nodeDistance = node.getDistance();
-            if(nodeDistance < min){
+            if (nodeDistance < min) {
                 min = nodeDistance;
                 returnedNode = node;
             }
@@ -247,7 +247,7 @@ public class Map {
 
     private void updateDistances(Node node1, int distance, Node node2) {
         int currentNodeDistance = node2.getDistance();
-        if(currentNodeDistance + distance < node1.getDistance()){
+        if (currentNodeDistance + distance < node1.getDistance()) {
             node1.setDistance(currentNodeDistance + distance);
             LinkedList<Node> shortestPath = new LinkedList<>(node2.getShortestPath());
             shortestPath.add(node2);
@@ -256,76 +256,76 @@ public class Map {
 
     }
 
-    public int getShortestDistance(Node fromNode, Node toNode){
+    public int getShortestDistance(Node fromNode, Node toNode) {
         LinkedList<Node> shortestPath = this.getShortestPath(fromNode, toNode);
         return toNode.getDistance();
     }
 
 
-    public void nDistance(Node fromNode, int distance, int tmpDistance){
+    public void nDistance(Node fromNode, int distance, int tmpDistance) {
         boolean result = false;
-        if (tmpDistance == 0){
+        if (tmpDistance == 0) {
             return;
         }
-        for (Node node : fromNode.getNeighbourNodes()){
-            if (node.getDistance() > (distance - tmpDistance)){
+        for (Node node : fromNode.getNeighbourNodes()) {
+            if (node.getDistance() > (distance - tmpDistance)) {
                 node.setDistance(distance - tmpDistance);
-                nDistance(node,distance,tmpDistance-1);
+                nDistance(node, distance, tmpDistance - 1);
 
             }
         }
     }
 
-    public boolean Distance(Node fromNode, Node toNode, int distance){
+    public boolean Distance(Node fromNode, Node toNode, int distance) {
         boolean result = false;
-        nDistance(fromNode,distance + 1,distance);
-        if (toNode.getDistance() <= distance){
+        nDistance(fromNode, distance + 1, distance);
+        if (toNode.getDistance() <= distance) {
             result = true;
         }
         return result;
     }
 
-    public Node isBetterThan(Node node1, Node node2, String type){
+    public Node isBetterThan(Node node1, Node node2, String type) {
         int count1stNode = 0;
         int count2ndNode = 0;
         String tmpType = null;
-        if (type.equals("OUVERTE")){
+        if (type.equals("OUVERTE")) {
             tmpType = "V";
         }
-        if (type.equals("GASTRONOMIQUE")){
+        if (type.equals("GASTRONOMIQUE")) {
             tmpType = "R";
         }
-        if (type.equals("CULTURELLE")){
+        if (type.equals("CULTURELLE")) {
             tmpType = "L";
         }
         Node nodeOpen = null;
         node1.setDistance(0);
         nDistance(node1, 3, 2);
-        for (Node node : this.nodes){
-            if ((node.getDistance() <= 2) && (node.getType().equals(tmpType))){
+        for (Node node : this.nodes) {
+            if ((node.getDistance() <= 2) && (node.getType().equals(tmpType))) {
                 count1stNode++;
             }
         }
-        for (Node node : this.nodes){
+        for (Node node : this.nodes) {
             node.setDistance(INFINITE);
         }
         node2.setDistance(0);
         nDistance(node2, 3, 2);
-        for (Node node : this.nodes){
-            if ((node.getDistance() <= 2) && (node.getType().equals(tmpType))){
+        for (Node node : this.nodes) {
+            if ((node.getDistance() <= 2) && (node.getType().equals(tmpType))) {
                 count2ndNode++;
             }
         }
 
-        if (count1stNode > count2ndNode){
+        if (count1stNode > count2ndNode) {
             nodeOpen = node1;
-        }else {
+        } else {
             nodeOpen = node2;
         }
         return nodeOpen;
     }
 
-    public LinkedList<Node> getPathWith(Node fromNode, Node throughNode, Node toNode){
+    public LinkedList<Node> getPathWith(Node fromNode, Node throughNode, Node toNode) {
         LinkedList<Node> firstPath = new LinkedList<>();
         LinkedList<Node> lastPath = new LinkedList<>();
 
@@ -338,7 +338,7 @@ public class Map {
     }
 
     //Find a path between two nodes through two given nodes
-    public LinkedList<Node> getPathWith(Node fromNode, Node toNode, Node throughNode1, Node throughNode2){ // Respect the order of the nodes ? (fromNode, throughNode1, throughNode2, toNode)
+    public LinkedList<Node> getPathWith(Node fromNode, Node toNode, Node throughNode1, Node throughNode2) { // Respect the order of the nodes ? (fromNode, throughNode1, throughNode2, toNode)
         LinkedList<Node> finalPath = new LinkedList<>();
         LinkedList<Node> finalPath2 = new LinkedList<>();
         LinkedList<Node> tempPath = new LinkedList<>();
@@ -363,117 +363,75 @@ public class Map {
         finalPath2.addAll(lastPath);
 
 
-        if(throughNode1.getDistance() + throughNode2.getDistance() + toNode.getDistance() < tmpDistance){
+        if (throughNode1.getDistance() + throughNode2.getDistance() + toNode.getDistance() < tmpDistance) {
             finalPath = finalPath2;
         }
 
         return finalPath;
     }
 
-<<<<<<< Updated upstream
+
     // Floyd-Warshall
-    public void floydWarshall(){
+    public void floydWarshall() {
         int[][] distance = new int[this.nodes.size()][this.nodes.size()];
         Node[][] predecessor = new Node[this.nodes.size()][this.nodes.size()];
-        for (int i = 0; i < this.nodes.size(); i++){
-            for (int j = 0; j < this.nodes.size(); j++){
-                if (i == j){
+        for (int i = 0; i < this.nodes.size(); i++) {
+            for (int j = 0; j < this.nodes.size(); j++) {
+                if (i == j) {
                     distance[i][j] = 0;
-                }else {
+                } else {
                     distance[i][j] = INFINITE;
                 }
             }
 
         }
 
-        for (int i = 0; i < this.nodes.size(); i++){
-            for (int j = 0; j < this.nodes.size(); j++){
-                if(nodes.get(i).isNeighbour(nodes.get(j))){
-                    if(nodes.get(i).getClosestNeighbour(nodes.get(j)).getDistance() < distance[i][j]){
+        for (int i = 0; i < this.nodes.size(); i++) {
+            for (int j = 0; j < this.nodes.size(); j++) {
+                if (nodes.get(i).isNeighbour(nodes.get(j))) {
+                    if (nodes.get(i).getClosestNeighbour(nodes.get(j)).getDistance() < distance[i][j]) {
                         distance[i][j] = nodes.get(i).getClosestNeighbour(nodes.get(j)).getDistance();
                         predecessor[i][j] = nodes.get(i);
                     }
                 }
             }
         }
+        // Initialisation marche pour sure
 
-        for (int i = 0; i < this.nodes.size(); i++){
-            for (int j = 0; j < this.nodes.size(); j++){
-                for (int k = 0; k < this.nodes.size(); k++){
-                    if(distance[i][k] != INFINITE && distance[k][j] != INFINITE){
-                        if (distance[i][j] > distance[i][k] + distance[k][j]){
-                            distance[i][j] = distance[i][k] + distance[k][j];
-                            predecessor[i][j] = predecessor[i][k];
+        for (int z = 0; z < this.nodes.size(); z++) { // 4 boucles ça fais beaucoup la non ???
+            for (int i = 0; i < this.nodes.size(); i++) {
+                for (int j = 0; j < this.nodes.size(); j++) {
+                    for (int k = 0; k < this.nodes.size(); k++) {
+                        if (distance[i][k] != INFINITE && distance[k][j] != INFINITE) {
+                            if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                                distance[i][j] = distance[i][k] + distance[k][j];
+                                predecessor[i][j] = predecessor[k][j]; // Cette ligne est fonctionnelle
+                            }
                         }
                     }
                 }
             }
         }
 
-        for (int i = 0; i < this.nodes.size(); i++){
-            for (int j = 0; j < this.nodes.size(); j++){
+        for (int i = 0; i < this.nodes.size(); i++) {
+            for (int j = 0; j < this.nodes.size(); j++) {
                 System.out.print(distance[i][j] + " ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
-
-        for (int i = 0; i < this.nodes.size(); i++){
-            for (int j = 0; j < this.nodes.size(); j++){
+        System.out.println("predecessor");
+        for (int i = 0; i < this.nodes.size(); i++) {
+            for (int j = 0; j < this.nodes.size(); j++) {
                 System.out.print(predecessor[i][j] + " ");
             }
-            System.out.println("");
-        }
-=======
-    // Floyd Warshall algorithm
-    public boolean floydWarshall(){
-        int i, j, k;
-        int n = this.nodes.size();
-        int[][] distance = new int[n][n];
-        int[][] predecessor = new int[n][n];
-        for (i = 0; i < n; i++){
-            for (j = 0; j < n; j++){
-                distance[i][j] = this.nodes.get(i).getNeighbours().get(j).getDistance(); // A changer
-                predecessor[i][j] = j;
-            }
-        }
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n; j++){
-                System.out.print(distance[i][j]);
-            }
-            System.out.println("");
-        }
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n; j++){
-                System.out.print(predecessor[i][j]);
-            }
-            System.out.println("");
+            System.out.println(" ");
         }
 
-        for (k = 0; k < n; k++){
-            for (i = 0; i < n; i++){
-                for (j = 0; j < n; j++){
-                    if (distance[i][k] + distance[k][j] < distance[i][j]){
-                        distance[i][j] = distance[i][k] + distance[k][j];
-                        predecessor[i][j] = predecessor[i][k];
-                    }
-                }
-            }
-        }
-        /*
-        for (i = 0; i < n; i++){
-            for (j = 0; j < n; j++){
-                if (distance[i][j] == INFINITE){
-                    this.nodes.get(i).setDistance(j, INFINITE);
-                }else {
-                    this.nodes.get(i).setDistance(j, distance[i][j]);
-                }
-            }
-        }
-        */
-        return false;
->>>>>>> Stashed changes
+
+
     }
 
 }
+
 
 
