06f75874696e2d0d4d9ba98202aa9f62bcb9c5c4
BASTIEN LE GALL <bastien.le-gall@etu.univ-lyon1.fr>
2022-06-01 21:08:40 +0200

    Added GraphDisplay class

--- src/app/main/gui/Window.java
+++ src/app/main/gui/Window.java
@@ -102,8 +102,9 @@ public class Window extends JFrame {
         panListeNoeud.setBorder(BorderFactory.createEtchedBorder());
 
 
-        initializeAffNoeuds();
         //panAffNoeuds.setBackground(Color.YELLOW);
+        GraphDisplay gd = new GraphDisplay(graph);
+        panAffNoeuds = gd.initializeAffNoeuds();
         panAffNoeuds.setBorder(BorderFactory.createEtchedBorder());
 
 
@@ -241,94 +242,4 @@ public class Window extends JFrame {
         return menubar;
     }
 
-    private void initializeAffNoeuds() {
-        ListenableGraph<String, DefaultEdge> g = new DefaultListenableGraph<>(new DirectedWeightedPseudograph<>(DefaultEdge.class));
-
-        for (Node node : graph.getNodes()) {
-            g.addVertex(node.toString());
-        }
-
-        for (Node node : graph.getNodes()) {
-            for (Link neighbour : node.getAllNeighbours()) {
-                g.setEdgeWeight(g.addEdge(node.toString(),
-                        neighbour.getNode().toString()), neighbour.getDistance());
-            }
-        }
-
-        JGraphXAdapter<String, DefaultEdge> jgxAdapter = new JGraphXAdapter<>(g);
-        mxGraphComponent graphComponent = new mxGraphComponent(jgxAdapter);
-        graphComponent.setConnectable(false);
-        graphComponent.setEnabled(false);
-
-
-        mxStylesheet stylesheet = new mxStylesheet();
-
-        Map<String, Object> defStyle = new HashMap<>();
-        defStyle.put(mxConstants.STYLE_FONTSIZE, "12");
-        defStyle.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_RECTANGLE);
-        defStyle.put(mxConstants.STYLE_PERIMETER, mxConstants.PERIMETER_RECTANGLE);
-
-        Map<String, Object> highlighted = defStyle;
-        highlighted.put(mxConstants.STYLE_LABEL_BORDERCOLOR, "#FFFF00");
-
-        // TODO : ajouter les styles pour les noeuds
-
-        mxFastOrganicLayout layout = new mxFastOrganicLayout(jgxAdapter);
-        layout.setForceConstant(200);
-        layout.execute(jgxAdapter.getDefaultParent());
-
-        // Make the graph zoomable and scrollable with ctrl-mousewheel
-        graphComponent.getGraphControl().addMouseWheelListener(new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if (e.isControlDown()) {
-                    if (e.getWheelRotation() < 0) {
-                        graphComponent.zoomIn();
-                    } else {
-                        graphComponent.zoomOut();
-                    }
-      }
-                // Normal scroll if control is not pressed
-                else if (e.isShiftDown()) {
-                    if (!e.isControlDown()) {
-                        graphComponent.getViewport().setViewPosition(new Point(
-                                graphComponent.getViewport().getViewPosition().x + e.getWheelRotation() * 15,
-                                graphComponent.getViewport().getViewPosition().y));
-                    } else {
-                        graphComponent.getViewport().setViewPosition(new Point(
-                                graphComponent.getViewport().getViewPosition().x - e.getWheelRotation() * 15,
-                                graphComponent.getViewport().getViewPosition().y));
-                    }
-                } else {
-                    if (!e.isControlDown()) {
-                        graphComponent.getViewport().setViewPosition(new Point(
-                                graphComponent.getViewport().getViewPosition().x,
-                                graphComponent.getViewport().getViewPosition().y + e.getWheelRotation() * 15));
-                    } else {
-                        graphComponent.getViewport().setViewPosition(new Point(
-                                graphComponent.getViewport().getViewPosition().x,
-                                graphComponent.getViewport().getViewPosition().y - e.getWheelRotation() * 15));
-                    }
-                }
-            }
-        });
-
-
-        graphComponent.getGraphControl().addMouseListener(new mxMouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                //Check if the mouse is over a cell
-                mxCell cell = ((mxCell) graphComponent.getCellAt(e.getX(), e.getY(), false));
-                if (cell != null) {
-                    //Check if the cell is a vertex
-                    if (graphComponent.getGraph().getModel().isVertex(cell)) {
-                        // TODO: Send the node to the right panel
-                        System.out.println(cell.getValue());
-                    }
-                }
-            }
-        });
-
-        this.panAffNoeuds = graphComponent;
-    }
 }
