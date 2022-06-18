ba13087361a5de2c4ea44cfe9002c86b695cb929
Mateo <mateo.guenot2003@gmail.com>
2022-06-18 15:18:38 +0200

    Class Test

---
+++ src/app/main/nodesTest/LinkTest.java
@@ -0,0 +1,59 @@
+package app.main.nodesTest;
+
+import app.main.map.Graph;
+import app.main.nodes.LinkType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LinkTest {
+    static Graph graph = new Graph();
+
+    Scanner input = new Scanner(System.in);
+
+    public LinkTest() {
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        String filename = null;
+        filename = "src/resources/test.csv";
+        graph.init(filename);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetLinkType() {
+        assertEquals(LinkType.NATIONALE, graph.getLinkFromString("N1").getType());
+    }
+
+    @Test
+    public void testGetLinkRoadNumber() {
+        assertEquals(1, graph.getLinkFromString("N1").getRoadNumber());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("N1", graph.getLinkFromString("N1").toString());
+    }
+
+    @Test
+    public void testDistance() {
+        assertEquals(10, graph.getLinkFromString("N1").getDistance());
+    }
+
+    @Test
+    public void testNeighbour() {
+        assertEquals(graph.getNodeFromString("V,b"),graph.getLinkFromString("N1").getNode());
+    }
+
+}
---
+++ src/app/main/nodesTest/NodeTest.java
@@ -0,0 +1,72 @@
+package app.main.nodesTest;
+
+import app.main.map.Graph;
+import app.main.nodes.LinkType;
+import app.main.nodes.Node;
+import app.main.nodes.NodeType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class NodeTest {
+    static Graph graph = new Graph();
+
+    Scanner input = new Scanner(System.in);
+
+    public NodeTest() {
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        String filename = null;
+        filename = "src/resources/test.csv";
+        graph.init(filename);
+    }
+
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetNodeType() {
+        assertEquals(NodeType.VILLE, graph.getNodeFromString("V,a").getType());
+    }
+
+    @Test
+    public void testGetNodeName() {
+        assertEquals("a", graph.getNodeFromString("V,a").getName());
+    }
+
+    @Test
+    public void testGetNodeId() {
+        assertEquals(0, graph.getNodeFromString("V,a").getId());
+    }
+
+    @Test
+    public void testNodeToString() {
+        assertEquals("V,a", graph.getNodeFromString("V,a").toString());
+    }
+
+    @Test
+    public void testIsNeighbour() {
+        assertTrue(graph.getNodeFromString("V,a").isNeighbour(graph.getNodeFromString("V,b")));
+    }
+
+    @Test
+    public void testFarthestNode() {
+        assertEquals(graph.getLinkFromString("N1"), graph.getNodeFromString("V,a").getFarthestNeighbour(graph.getNodeFromString("V,b")));
+    }
+
+    @Test
+    public void testClosestNeighbour() {
+        assertEquals(graph.getLinkFromString("N1"), graph.getNodeFromString("V,a").getClosestNeighbour(graph.getNodeFromString("V,b")));
+    }
+
+}
